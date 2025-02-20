ba26df7c1b22c3b7b8056c74454637556fb6579c
mdecke <mdecke@student.ethz.ch>
2025-02-20 10:40:54 -0800

    plotting with new plotter

--- Tests/DDPGMetricsPlotting.py
+++ Tests/DDPGMetricsPlotting.py
@@ -6,27 +6,16 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from MiscFunctions.Plotting import *
 
 plt.figure(figsize=(15, 8))
+fig, ax = plt.subplots(3, 1, sharex=False, figsize=(12, 10))
 
-oh_ddpg = DDPGMetrics(file_path='Data/CSVs/OHNoiseTraining.csv', show=False, title='OH Noise', smooth=True)
-oh_ddpg.plot_losses()
+gauss_plotter = DDPGMetrics(file_path='Data/CSVs/Metrics/Gaussian.csv', show=False, title='Gaussian added Noise', smooth=20)
+gauss_plotter.plot_losses(ax=ax)
 
-pa_ddpg = DDPGMetrics(file_path='Data/CSVs/P(a)NoiseTraining.csv', show=False, title='P(a) Noise', smooth=True)
-pa_ddpg.plot_losses()
-
-pas_ddpg = DDPGMetrics(file_path='Data/CSVs/P(a|s)NoiseTraining.csv', show=False, title='P(a[k]|s[k]) Noise', smooth=True)
-pas_ddpg.plot_losses()
-
-pasa_ddpg = DDPGMetrics(file_path='Data/CSVs/P(a|s,a)NoiseTraining.csv', show=False, title='P(a[k]|s[k],a[k-1]) Noise', smooth=True)
-pasa_ddpg.plot_losses()
-
-pas_MLE_ddpg = DDPGMetrics(file_path='Data/CSVs/P(a|s)NoiseTraining_MLE.csv', show=False, title='P(a[k]|s[k])MLE Noise', smooth=True)
-pas_MLE_ddpg.plot_losses()
-
-pasa_MLE_ddpg = DDPGMetrics(file_path='Data/CSVs/P(a|s,a)NoiseTraining_MLE.csv', show=False, title='P(a[k]|s[k],a[k-1])MLE Noise', smooth=True)
-pasa_MLE_ddpg.plot_losses()
+ou_plotter = DDPGMetrics(file_path='Data/CSVs/Metrics/OrnsteinUhlenbeck.csv', show=False, title='Ornstein-Uhlenbeck added Noise', smooth=20)
+ou_plotter.plot_losses(ax=ax)
 
 plt.tight_layout()
-plt.savefig('Data/Plots/ALLTrainingComparison.svg')
+# plt.savefig('Data/Plots/ALLTrainingComparison.svg')
 plt.show()
 
 
