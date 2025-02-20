37c8b6f0a48419bf58fef55f40c46d37968eaea2
mdecke <mdecke@student.ethz.ch>
2025-02-20 11:15:18 -0800

    fixed extra fig

--- Tests/DDPGMetricsPlotting.py
+++ Tests/DDPGMetricsPlotting.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from MiscFunctions.Plotting import *
 
-plt.figure(figsize=(15, 8))
-fig, ax = plt.subplots(3, 1, sharex=False, figsize=(12, 10))
+fig, ax = plt.subplots(3, 1, sharex=False, figsize=(10, 10))
 
 gauss_plotter = DDPGMetrics(file_path='Data/CSVs/Metrics/Gaussian.csv', show=False, title='Gaussian added Noise', smooth=20)
 gauss_plotter.plot_losses(ax=ax)
