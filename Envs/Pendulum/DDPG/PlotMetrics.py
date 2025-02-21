80125f9a8758c29204bd679f67446afd9d73daf0
mdecke <mdecke@student.ethz.ch>
2025-02-21 14:57:58 -0800

    new data file to plot

--- Envs/Pendulum/DDPG/PlotMetrics.py
+++ Envs/Pendulum/DDPG/PlotMetrics.py
@@ -4,11 +4,11 @@ from MiscFunctions.Plotting import *
 
 fig, ax = plt.subplots(3, 1, sharex=False, figsize=(15, 8))
 
-gauss_plotter = DDPGMetrics(file_path='Data/CSVs/Metrics/tests/Gaussian.csv', show=False, title='Gaussian added Noise')
+gauss_plotter = DDPGMetrics(file_path='Envs/Pendulum/DDPG/data/metrics.csv', show=False, title='Gaussian added Noise')
 gauss_plotter.plot_losses(ax=ax)
 
-ou_plotter = DDPGMetrics(file_path='Data/CSVs/Metrics/tests/OrnsteinUhlenbeck.csv', show=False, title='Ornstein-Uhlenbeck added Noise')
-ou_plotter.plot_losses(ax=ax)
+# ou_plotter = DDPGMetrics(file_path='Data/CSVs/Metrics/tests/OrnsteinUhlenbeck.csv', show=False, title='Ornstein-Uhlenbeck added Noise')
+# ou_plotter.plot_losses(ax=ax)
 
 plt.tight_layout()
 # plt.savefig('Data/Plots/ALLTrainingComparison.svg')
